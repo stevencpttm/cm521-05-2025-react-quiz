8fe4aacfbbe9f2e0759861d2fb046c14d25d5379
stevencpttm <56670584+stevencpttm@users.noreply.github.com>
2025-05-22 20:35:05 +0800

    class component -> function component

--- src/App.js
+++ src/App.js
@@ -1,82 +1,76 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import "./App.css";
 
-class App extends Component {
-  state = {
-    topics: [
-      {
-        question: "JavaScript 與 Java 有什麼關係？",
-        answers: [
-          {
-            value: "同公司的產品",
-            correct: false,
-          },
-          {
-            value: "新版與舊版的關係",
-            correct: false,
-          },
-          {
-            value: "一點關係也沒有",
-            correct: true,
-          },
-          {
-            value: "JavaScript 是 Java 的 Web 版本",
-            correct: false,
-          },
-        ],
-      },
-      {
-        question: "發明 React JS 的公司是？",
-        answers: [
-          {
-            value: "Google",
-            correct: false,
-          },
-          {
-            value: "Facebook",
-            correct: true,
-          },
-          {
-            value: "Apple",
-            correct: false,
-          },
-          {
-            value: "Microsoft",
-            correct: false,
-          },
-        ],
-      },
-    ],
-    currentIndex: 0,
-    corrects: 0,
-    buttonClasses: ["", "", "", ""],
-  };
+function App() {
+  const [topics, setTopics] = useState([
+    {
+      question: "JavaScript 與 Java 有什麼關係？",
+      answers: [
+        {
+          value: "同公司的產品",
+          correct: false,
+        },
+        {
+          value: "新版與舊版的關係",
+          correct: false,
+        },
+        {
+          value: "一點關係也沒有",
+          correct: true,
+        },
+        {
+          value: "JavaScript 是 Java 的 Web 版本",
+          correct: false,
+        },
+      ],
+    },
+    {
+      question: "發明 React JS 的公司是？",
+      answers: [
+        {
+          value: "Google",
+          correct: false,
+        },
+        {
+          value: "Facebook",
+          correct: true,
+        },
+        {
+          value: "Apple",
+          correct: false,
+        },
+        {
+          value: "Microsoft",
+          correct: false,
+        },
+      ],
+    },
+  ]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [corrects, setCorrects] = useState(0);
+  const [buttonClasses, setButtonClasses] = useState(["", "", "", ""]);
 
-  answer = (index) => {
-    const question = this.state.topics[this.state.currentIndex];
+  const next = (index) => {
+    const question = topics[currentIndex];
     const answered = question.answers[index];
 
     if (answered.correct) {
       // corrects +1
-      this.setState({
-        corrects: this.state.corrects + 1,
-      });
+      setCorrects(corrects + 1);
     }
 
     // find the correct index
     const correctIndex = question.answers.findIndex((answer) => answer.correct);
-    this.highlightButton(index, correctIndex);
+    highlightButton(index, correctIndex);
 
     // switch to next question after 1 second
     setTimeout(() => {
-      this.setState({
-        currentIndex: this.state.currentIndex + 1,
-        buttonClasses: new Array(4).fill(""),
-      });
+      setCurrentIndex(currentIndex + 1);
+      setButtonClasses(new Array(4).fill(""));
     }, 1000);
   };
 
-  highlightButton = (selectedIndex, correctIndex) => {
+  const highlightButton = (selectedIndex, correctIndex) => {
     const classes = new Array(4).fill("");
 
     if (selectedIndex === correctIndex) {
@@ -86,68 +80,55 @@ class App extends Component {
       classes[correctIndex] = "correct";
     }
 
-    this.setState({
-      buttonClasses: classes,
-    });
+    setButtonClasses(classes);
   };
 
-  startOver = () => {
-    this.setState({
-      currentIndex: 0,
-      corrects: 0,
-      buttonClasses: new Array(4).fill(""),
-    });
+  const startOver = () => {
+    setCurrentIndex(0);
+    setCorrects(0);
+    setButtonClasses(new Array(4).fill(""));
   };
 
-  render() {
-    const topic = this.state.topics[this.state.currentIndex];
+  const topic = topics[currentIndex];
 
-    return (
-      <div className="App">
-        <div
-          className="statusBar"
-          style={{
-            width: `${
-              (this.state.currentIndex / this.state.topics.length) * 100
-            }%`,
-          }}
-        ></div>
+  return (
+    <div className="App">
+      <div
+        className="statusBar"
+        style={{
+          width: `${(currentIndex / topics.length) * 100}%`,
+        }}
+      ></div>
 
-        {typeof topic !== "undefined" ? (
-          <div className="topics-container">
-            <h2>{topic.question}</h2>
+      {typeof topic !== "undefined" ? (
+        <div className="topics-container">
+          <h2>{topic.question}</h2>
 
-            {topic.answers.map((answer, index) => {
-              return (
-                <button
-                  className={this.state.buttonClasses[index]}
-                  key={index}
-                  onClick={() => this.answer(index)}
-                >
-                  {answer.value}
-                </button>
-              );
-            })}
+          {topic.answers.map((answer, index) => {
+            return (
+              <button
+                className={buttonClasses[index]}
+                key={index}
+                onClick={() => next(index)}
+              >
+                {answer.value}
+              </button>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="fireworks">
+          <div className="before"></div>
+          <div className="after"></div>
+          <div className="result">
+            <h2>Completed!</h2>
+            <h3>Your score: {Math.round((corrects / topics.length) * 100)}</h3>
+            <button onClick={startOver}>Start Over</button>
           </div>
-        ) : (
-          <div className="fireworks">
-            <div className="before"></div>
-            <div className="after"></div>
-            <div className="result">
-              <h2>Completed!</h2>
-              <h3>
-                Your score:{" "}
-                {Math.round(
-                  (this.state.corrects / this.state.topics.length) * 100
-                )}
-              </h3>
-              <button onClick={this.startOver}>Start Over</button>
-            </div>
-          </div>
-        )}
-      </div>
-    );
-  }
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
